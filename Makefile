91e9ec95ab365fe43a1daae5171b583fe9324a0a
soonann <mail@soonann.dev>
2024-05-04 19:04:37 +0800

    feat: lvl 1
    Signed-off-by: soonann <mail@soonann.dev>

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 .DEFAULT = run
+FLAGS = -Wall -Weverything
 
 run: build
 	./main
 
 build:
-	clang main.c -o main -Wall -Wpedantic
+	clang main.c -o main $(FLAGS)
 
 clean:
 	rm main
--- main.c
+++ main.c
@@ -1,10 +1,35 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
 
-void showps1() {
-  char buf[256];
-  getcwd(buf, sizeof(buf));
-  printf("%s$ \n", buf);
+#define MAX_INPUT_BYTES 256
+#define MAX_PATH_BYTES 256
+
+// shows the terminal prompt
+void prompt() {
+  char buf[MAX_PATH_BYTES];
+  getcwd(buf, MAX_PATH_BYTES);
+  printf("%s$ ", buf);
 }
 
-int main() { showps1(); }
+// main function
+int main() {
+  bool quit = false;
+  char buf[MAX_INPUT_BYTES];
+
+  // handle EOF
+  while (!feof(stdin)) {
+
+    // show prompt
+    prompt();
+
+    // get value from user
+    char *s = fgets(buf, MAX_INPUT_BYTES, stdin);
+    if (s == NULL) {
+      printf("exited shell \n");
+      return 1;
+    }
+  }
+
+  return 0;
+}
