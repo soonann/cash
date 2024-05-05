2055302b8f6e1c9e0d95c0b1d6723346a09dd26a
soonann <mail@soonann.dev>
2024-05-05 21:10:17 +0800

    chore: clang-format intial
    Signed-off-by: soonann <mail@soonann.dev>

--- crunchy.c
+++ crunchy.c
@@ -10,84 +10,84 @@
 #define MAX_PATH_BYTES 256
 #define MAX_CMD_ARGS 64
 
-// given a char arr, split it into an arr of char arr
-char **parse_input(char *input, unsigned int n) {
+// Given a char array, split it into an array of char array(s)
+char **parse_input(char *input, unsigned int n)
+{
+	// Init a 2d array
+	char **cmd = malloc(MAX_CMD_ARGS * sizeof(char *));
+	char **head = cmd;
 
-  // init a 2d array
-  char **cmd = malloc(MAX_CMD_ARGS * sizeof(char *));
-  char **head = cmd;
+	*cmd = strtok(input, " ");
+	while (*cmd != NULL) {
+		// Only shift the pointer when it is not an empty string
+		if (strlen(*cmd)) {
+			cmd++;
+		}
+		*cmd = strtok(NULL, " ");
+	}
 
-  *cmd = strtok(input, " ");
-  while (*cmd != NULL) {
-    // only shift the pointer when the value isn't a whitespace
-    if (strlen(*cmd) > 1) {
-      cmd++;
-    }
-    *cmd = strtok(NULL, " ");
-  }
-
-  return head;
+	return head;
 }
 
-// main function
-int main() {
-  char buf[MAX_INPUT_BYTES];
+// Main function
+int main()
+{
+	char buf[MAX_INPUT_BYTES];
 
-  // handle EOF
-  while (!feof(stdin)) {
+	// Handle EOF
+	while (!feof(stdin)) {
+		// get cwd
+		char cwd[MAX_PATH_BYTES];
+		getcwd(cwd, MAX_PATH_BYTES);
+		printf("%s$ ", cwd);
 
-    // get cwd
-    char cwd[MAX_PATH_BYTES];
-    getcwd(cwd, MAX_PATH_BYTES);
-    printf("%s$ ", cwd);
+		// TODO: handle the length properly instead of just using
+		// MAX_INPUT_BYTES
+		char *s = fgets(buf, MAX_INPUT_BYTES, stdin);
+		if (s == NULL) {
+			printf("exited shell \n");
+			return 1;
+		}
 
-    // TODO: handle the length properly instead of just using
-    // MAX_INPUT_BYTES
-    char *s = fgets(buf, MAX_INPUT_BYTES, stdin);
-    if (s == NULL) {
-      printf("exited shell \n");
-      return 1;
-    }
+		// parse the input into command and args
+		char **inputs = parse_input(s, strlen(s));
 
-    // parse the input into command and args
-    char **inputs = parse_input(s, strlen(s));
+		// builtins
+		if (!strcmp(inputs[0], "exit")) {
+			if (inputs[1] == NULL) {
+				return 0;
+			}
 
-    // builtins
-    if (!strcmp(inputs[0], "exit")) {
-      if (inputs[1] == NULL) {
-        return 0;
-      }
+			int code = atoi(inputs[1]);
+			int err = my_exit(code);
+			if (err == -1) {
+				fputs(strerror(errno), stdout);
+			}
 
-      int code = atoi(inputs[1]);
-      int err = my_exit(code);
-      if (err == -1) {
-        fputs(strerror(errno), stdout);
-      }
+			return err;
+		} else if (!strcmp(inputs[0], "cd")) {
+			int err = my_cd(inputs[1]);
+			if (err == -1) {
+				printf("cd: %s\n", strerror(errno));
+			}
+		} else if (!strcmp(inputs[0], "exec")) {
+			int err = my_exec(inputs + 1);
+			if (err == -1) {
+				printf("exec: %s\n", strerror(errno));
+			}
 
-      return err;
-    } else if (!strcmp(inputs[0], "cd")) {
-      int err = my_cd(inputs[1]);
-      if (err == -1) {
-        printf("cd: %s\n", strerror(errno));
-      }
-    } else if (!strcmp(inputs[0], "exec")) {
-      int err = my_exec(inputs + 1);
-      if (err == -1) {
-        printf("exec: %s\n", strerror(errno));
-      }
+		} else {
+			// TODO: fix this as well, why funky while loops?
+			printf("Unrecognized command: %s\n", inputs[0]);
+		}
 
-    } else {
-      // TODO: fix this as well, why funky while loops?
-      printf("Unrecognized command: %s\n", inputs[0]);
-    }
+		// print the output of the shell
+		/* char **tmp = inputs; */
+		/* while (*tmp != NULL) { */
+		/*   printf("line is: %s\n", *tmp); */
+		/*   tmp++; */
+		/* } */
+	}
 
-    // print the output of the shell
-    /* char **tmp = inputs; */
-    /* while (*tmp != NULL) { */
-    /*   printf("line is: %s\n", *tmp); */
-    /*   tmp++; */
-    /* } */
-  }
-
-  return 0;
+	return 0;
 }
--- my_builtins.c
+++ my_builtins.c
@@ -1,10 +1,19 @@
 #include <unistd.h>
 
-// exits the current process with the given code
-int my_exit(int code) { return code; }
+// Exits the current process with the given exit code
+int my_exit(int code)
+{
+	return code;
+}
 
-// changes the current directory to the given str
-int my_cd(char *path) { return chdir(path); }
+// Changes the current directory to the given path
+int my_cd(char *path)
+{
+	return chdir(path);
+}
 
-// executes the given function
-int my_exec(char **params) { return execv(params[0], params + 1); }
+// Executes the given function with execv
+int my_exec(char **params)
+{
+	return execv(params[0], params + 1);
+}
